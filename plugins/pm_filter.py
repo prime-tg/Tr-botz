4ff74d2be242e3ff68f901b7ad5f2da745dc9a4b
Loganathan <143515274+Lovely-Logu@users.noreply.github.com>
2023-11-10 23:32:15 +0530

    Update pm_filter.py ᴠ2

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -50,7 +50,14 @@ async def pm_next_page(bot, query):
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'pmfile#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'pmfile#{file.file_id}')] for file in files ]
 
-    btn.insert(0, [InlineKeyboardButton("🔗 ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🔗", "howdl")])
+    btn.insert(0, [InlineKeyboardButton("mov", "{}")])
+    btn.insert(0, [
+             InlineKeyboardButton("Join Channel", url="https://t.me/Tr_LinksZz"),
+             InlineKeyboardButton("ᴍɪɴғᴏ", callback_data="button1"),
+             InlineKeyboardButton("sɪɴғᴏ", callback_data="button2"),
+             InlineKeyboardButton("ɪɴғᴏ ", callback_data="button2"),
+             InlineKeyboardButton("ᴛɪᴘɪɴғᴏ", callback_data="button2")
+        ])
     if 0 < offset <= 10: off_set = 0
     elif offset == 0: off_set = None
     else: off_set = offset - 10
@@ -124,7 +131,14 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
             btn = [[InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'{pre}#{req}#{file.file_id}'),
                     InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'{pre}#{file.file_id}')] for file in files ]    
 
-    btn.insert(0, [InlineKeyboardButton("🔗 ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🔗", "howdl")])
+    btn.insert(0, [InlineKeyboardButton("mov", "{}")])
+    btn.insert(0, [
+             InlineKeyboardButton("Join Channel", url="https://t.me/Tr_LinksZz"),
+             InlineKeyboardButton("ᴍɪɴғᴏ", callback_data="button1"),
+             InlineKeyboardButton("sɪɴғᴏ", callback_data="button2"),
+             InlineKeyboardButton("ɪɴғᴏ ", callback_data="button2"),
+             InlineKeyboardButton("ᴛɪᴘɪɴғᴏ", callback_data="button2")
+        ])
     if offset != "":
         key = f"{message.id}"
         temp.PM_BUTTONS[key] = search
