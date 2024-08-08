0180d95fd793808b8ab8444601eaeb00ad0ad4db
prime-tg <trmainautofilterbot@gmail.com>
2024-08-08 20:41:31 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -44,7 +44,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer(text="👋 ʜᴇʏ ғʀɪᴇɴᴅs 😻\n\n🎯 ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛʜɪs ғɪʟᴇs ʏᴏᴜ ᴡᴀɴᴛ ᴀɴᴅ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n\n❤️‍🔥 𝐌𝐚𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : @Tr_Movies_Links ❤️‍🔥", show_alert=True)
         
     elif query.data == "info":
-        await query.answer(text="⚠️ 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 ⚠️\n\nᴀғᴛᴇʀ 5 ᴍɪɴᴜᴛᴇs ᴛʜɪs ᴍᴇssᴀɢᴇs ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ\n\nɪғ ʏᴏᴜ ᴅᴏ ɴᴏᴛ sᴇᴇ ᴛʜᴇ ʀᴇǫᴜᴇsᴛᴇᴅ ᴀɴɪᴍᴇ ᴍᴏᴠɪᴇs sᴇʀɪᴇs ғɪʟᴍ, ʟᴏᴏᴋ ᴀᴛ ᴛʜᴇ ɴᴇxᴛ ᴘᴀɢᴇ\n\n❤️‍🔥 𝐌𝐚𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : @Tr_Movies_Links ❤️‍🔥", show_alert=True)
+        await query.answer(text="⚠️ 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 ⚠️\n\nᴀғᴛᴇʀ 5 ᴍɪɴᴜᴛᴇs ᴛʜɪs ᴍᴇssᴀɢᴇs ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ\n\nɪғ ʏᴏᴜ ᴅᴏ ɴᴏᴛ sᴇᴇ ᴛʜᴇ ʀᴇǫᴜᴇsᴛᴇᴅ ᴀɴɪᴍᴇ ᴍᴏᴠɪᴇs sᴇʀɪᴇs ғɪʟᴍ, ʟᴏᴏᴋ ᴀᴛ ᴛʜᴇ ɴᴇxᴛ ᴘᴀɢᴇ\n\n❤️‍🔥 @Tr_Movies_Links ❤️‍🔥", show_alert=True)
         
     elif query.data == "movie":
         await query.answer(text="🎬 𝗠𝗼𝘃𝗶𝗲𝘀 𝗥𝗲𝗾𝘂𝗲𝘀𝘁 𝗙𝗼𝗿𝗺𝗮𝘁 🎬\n\n➠ ɢᴏ ᴛᴏ ɢᴏᴏɢʟᴇ\n➠ ᴛʏᴘᴇ ᴍᴏᴠɪᴇ ɴᴀᴍᴇ\n➠ ᴄᴏᴘʏ ᴄᴏʀʀᴇᴄᴛ ɴᴀᴍᴇ\n➠ ᴘᴀsᴛᴇ ᴛʜɪs ɢʀᴏᴜᴘ\n\nᴇxᴀᴍᴘʟᴇ : Varisu Tamil\n\n🚯 ᴅᴏɴᴛ ᴜsᴇ ':(!,./)\n\n❤️‍🔥 𝐌𝐚𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : @Tr_Movies_Links ❤️‍🔥", show_alert=True)
